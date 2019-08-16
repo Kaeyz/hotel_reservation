8981f1d2bee07882210065a6398e8e6a9fa32d51
Agbele_Kolade <kolagbele@gmail.com>
2019-08-17 00:09:12 +0200

    express setup and initialized

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const dotenv = require("dotenv");
-
-
+const express = require("express");
 
 
 // configure variable.env
 dotenv.config({ path: ".env" });
 
 
+//config files
+const keys = require("./config/keys");
+
+
+// initialize express
+const app = express();
+
+
+
+
+
 
 
 const port = keys.port;
+
+app.listen(port, () => console.log(`server running on ${keys.host}:${port}`));
