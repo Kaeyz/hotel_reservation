5cf1daca7145dba10773ee1e14d81ed91593c9d7
Agbele_Kolade <kolagbele@gmail.com>
2019-08-18 22:50:50 +0200

    enviroment varibles for admin_server setup

---
+++ admin_server/config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  nodeEnv: process.env.NODE_ENV,
+	mongoURI: process.env.mongoURI,
+	port: process.env.PORT,
+  host: process.env.HOST,
+  secretOrKey: process.env.secretOrKey
+}
---
+++ admin_server/server.js
@@ -0,0 +1,7 @@
+const dotenv    = require("dotenv");
+
+
+
+
+// configure variable.env
+dotenv.config({ path: ".env" });
