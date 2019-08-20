44c8f0465d9a40ad0a6653646b8e3c125d77178c
Agbele_Kolade <kolagbele@gmail.com>
2019-08-20 12:33:19 +0200

    room schema added

---
+++ user_server/components/Rooms/Rooms.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const RoomSchema = new Schema({
+	name: {
+		type: String,
+		unique: true,
+		lowercase: true,
+		trim: true,
+		required: "name is required"
+  },
+  available_amount: {
+    type: Number,
+		required: true,
+		default: 1
+  },
+  requiredPoints: {
+		type: Number,
+		required: true,
+		default: 250
+	}
+});
+
+module.exports = mongoose.model("rooms", RoomSchema);
